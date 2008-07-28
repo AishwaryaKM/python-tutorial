6548f07b5624c7e19101089478e6a9c3596654e2
Mark Seaborn <mrs@mythic-beasts.com>
2008-07-28 21:07:02 +0100

    Initial version of CapPython verifier:  * Annotates AST with variable binding information.  * Implements some simple checks about attribute access.

---
+++ lint.py
@@ -0,0 +1,268 @@
+
+from compiler import ast
+
+
+class Binding(object):
+
+    def __init__(self, name, is_global):
+        self.name = name
+        self.is_global = is_global
+        self.is_self_var = False
+
+    def __repr__(self):
+        return "<Binding 0x%x %r global=%s>" % (
+            id(self), self.name, self.is_global)
+
+
+class Environ(object):
+
+    def __init__(self, env, bindings, global_vars):
+        self._env = env
+        self._bindings = bindings
+        self._global_vars = global_vars
+
+    def lookup(self, name):
+        return self._env[name]
+
+    def _get(self, name):
+        if name in self._env:
+            return self._env[name]
+        else:
+            if name not in self._global_vars:
+                self._global_vars[name] = Binding(name, is_global=True)
+            return self._global_vars[name]
+
+    def record(self, node, name):
+        node.binding = self._get(name)
+
+    def bind(self, name):
+        binding = Binding(name, is_global=False)
+        env = self._env.copy()
+        env[name] = binding
+        self._bindings.append(binding)
+        return Environ(env, self._bindings, self._global_vars)
+
+    def set_global(self, name):
+        # Could add to global_vars too
+        env = self._env.copy()
+        del env[name]
+        return Environ(env, self._bindings, self._global_vars)
+
+    def get_global_scope(self):
+        return Environ({}, self._bindings, self._global_vars)
+
+
+class Node(object):
+
+    def __init__(self, node):
+        self._node = node
+
+    def assigned(self, var_set):
+        raise NotImplementedError()
+
+    def find_globals(self, var_set):
+        raise NotImplementedError()
+
+    # env is the environment that nodes get when evaluated,
+    # but cenv is the environment that closures capture.
+    # The two are only different in class scope.
+    def annotate(self, env, cenv):
+        raise NotImplementedError()
+
+    def is_self_var(self):
+        return False
+
+
+class HandleName(Node):
+
+    def assigned(self, var_set):
+        pass
+
+    def find_globals(self, var_set):
+        pass
+
+    def annotate(self, env, cenv):
+        env.record(self._node, self._node.name)
+
+    def is_self_var(self):
+        return self._node.binding.is_self_var
+
+
+class HandleAssName(Node):
+
+    def assigned(self, var_set):
+        assert self._node.flags == "OP_ASSIGN"
+        var_set.add(self._node.name)
+
+    def find_globals(self, var_set):
+        pass
+
+    def annotate(self, env, cenv):
+        env.record(self._node, self._node.name)
+
+
+class HandleAugAssign(Node):
+
+    def assigned(self, var_set):
+        assert isinstance(self._node.node, ast.Name)
+        var_set.add(self._node.node.name)
+
+    def find_globals(self, var_set):
+        pass
+
+    def annotate(self, env, cenv):
+        for node in self._node.getChildNodes():
+            map_node(node).annotate(env, cenv)
+
+
+class HandleFunction(Node):
+
+    def assigned(self, var_set):
+        var_set.add(self._node.name)
+        # Function starts new scope, so assignments in body are hidden
+
+    def find_globals(self, var_set):
+        # Function starts new scope, so global decls in body are hidden
+        pass
+
+    def annotate(self, env, cenv):
+        env.record(self._node, self._node.name)
+        for default in self._node.defaults:
+            map_node(default).annotate(env)
+        global_vars = find_globals(self._node.code)
+        assigned_vars = find_assigned(self._node.code)
+        for var in assigned_vars:
+            cenv = cenv.bind(var)
+        for var in global_vars:
+            cenv = cenv.set_global(var)
+        # TODO: pattern args
+        for var in self._node.argnames:
+            assert var not in global_vars
+            cenv = cenv.bind(var)
+        self._node.code.environ = cenv
+        map_node(self._node.code).annotate(cenv, cenv)
+
+
+class HandleClass(Node):
+
+    def assigned(self, var_set):
+        var_set.add(self._node.name)
+        # No recurse
+
+    def find_globals(self, var_set):
+        # No recurse
+        pass
+
+    def annotate(self, env, cenv):
+        env.record(self._node, self._node.name)
+        for base in self._node.bases:
+            map_node(base).annotate(env, cenv)
+        # Classes have weird-assed scoping rules.
+        # Classes do not behave according to lexical scope!
+        # Assigned variables are not added to cenv.
+        # Assigned variables' values default to those of their namesakes
+        # in global (not enclosing) scope.
+        env = env.get_global_scope()
+        for var in find_assigned(self._node.code):
+            # Approximation
+            env = env.bind(var)
+        map_node(self._node.code).annotate(env, cenv)
+
+
+class HandleGlobal(Node):
+
+    def assigned(self, var_set):
+        pass
+
+    def find_globals(self, var_set):
+        var_set.update(self._node.names)
+
+    def annotate(self, env, cenv):
+        pass
+
+
+class HandleBoring(Node):
+
+    def assigned(self, var_set):
+        for node in self._node.getChildNodes():
+            map_node(node).assigned(var_set)
+
+    def find_globals(self, var_set):
+        for node in self._node.getChildNodes():
+            map_node(node).find_globals(var_set)
+
+    def annotate(self, env, cenv):
+        for node in self._node.getChildNodes():
+            map_node(node).annotate(env, cenv)
+
+
+node_types = {
+    "Name": HandleName,
+    "AssName": HandleAssName,
+    "AugAssign": HandleAugAssign,
+    "Function": HandleFunction,
+    "Class": HandleClass,
+    "Global": HandleGlobal,
+    }
+for ty in ("Stmt", "Assign", "AssTuple", "Const", "AssAttr", "Discard",
+           "CallFunc", "Getattr", "Return", "List"):
+    assert ty not in node_types
+    node_types[ty] = HandleBoring
+
+
+def map_node(node):
+    return node_types[node.__class__.__name__](node)
+
+
+def find_assigned(node):
+    var_set = set()
+    map_node(node).assigned(var_set)
+    return var_set
+
+def find_globals(node):
+    var_set = set()
+    map_node(node).find_globals(var_set)
+    return var_set
+
+
+def annotate(node):
+    bindings = []
+    global_vars = {}
+    env = Environ({}, bindings, global_vars)
+    map_node(node).annotate(env, env)
+    return set(global_vars.iterkeys())
+
+
+def find_all(node, node_type):
+    got = []
+
+    def recurse(node):
+        if isinstance(node, node_type):
+            got.append(node)
+        for subnode in node.getChildNodes():
+            recurse(subnode)
+
+    recurse(node)
+    return got
+
+
+def is_private_attr(name):
+    return name.startswith("_")
+
+
+def check(tree, vars):
+    log = []
+    for class_node in find_all(tree, ast.Class):
+        for defn in class_node.code.nodes:
+            if isinstance(defn, ast.Function):
+                if defn.decorators is None:
+                    binding = defn.code.environ.lookup(defn.argnames[0])
+                    binding.is_self_var = True
+    for node in find_all(tree, ast.AssAttr):
+        if not map_node(node.expr).is_self_var():
+            log.append(("SetAttr", node))
+    for node in find_all(tree, ast.Getattr):
+        if (not map_node(node.expr).is_self_var() and
+            is_private_attr(node.attrname)):
+            log.append(("GetAttr", node))
+    return log
---
+++ lint_test.py
@@ -0,0 +1,218 @@
+
+from compiler import ast
+import compiler
+import unittest
+
+import lint
+
+
+def parse_statement(string):
+    tree = compiler.parse(string)
+    assert isinstance(tree, ast.Module)
+    return tree.node
+
+
+def find_expected_errors(text):
+    for index, line in enumerate(text.split("\n")):
+        if "FAIL:" in line:
+            error = line.split("FAIL:", 1)[1]
+            yield error.strip(), index + 1
+
+
+# Intended to be used as a decorator.
+# This inverts the test.  We do not expect the test to pass.
+def TODO_test(method):
+    def wrapper(self):
+        self.assertRaises(AssertionError, lambda: method(self))
+    wrapper.__name__ = method.__name__
+    return wrapper
+
+
+class LintTest(unittest.TestCase):
+
+    def test_find_assigned(self):
+        tree = parse_statement("""
+x = foo1
+y = foo2
+a, b = foo3
+c += foo3
+d = e = foo4
+def func(arg):
+    f = foo5
+""")
+        self.assertEquals(lint.find_assigned(tree),
+                          set(["x", "y", "a", "b", "c", "d", "e", "func"]))
+
+    def test_find_globals(self):
+        tree = parse_statement("""
+global a
+global b, c
+def func(arg):
+    global d
+""")
+        self.assertEquals(lint.find_globals(tree),
+                          set(["a", "b", "c"]))
+        # Check that find_globals() corresponds to Python's scoping behaviour.
+        eval(compiler.compile("""
+x = 1
+def func():
+    x = "not seen"
+func()
+assert x == 1, x
+
+def func():
+    x = 2
+    global x
+func()
+assert x == 2, x
+
+x = 1
+def func():
+    def func2():
+        # This declaration does not escape to affect func().
+        global x
+    x = "not seen"
+func()
+assert x == 1, x
+
+def func():
+    global x
+    def func2():
+        x = 3
+    func2()
+func()
+assert x == 1, x
+
+def func():
+    # This declaration does not leak into func2().
+    global x
+    def func2():
+        x = 3
+    func2()
+func()
+assert x == 1, x
+
+def func(x):
+    def func2():
+        global x
+        def func3():
+            assert x == 1
+        func3()
+    func2()
+func(100)
+assert x == 1, x
+
+def func():
+    global x
+    def func2(x):
+        assert x == 100
+    func2(100)
+func()
+""", "filename", "exec"), {})
+
+    def test_free_vars(self):
+        def free_vars(text):
+            return lint.annotate(parse_statement(text))
+        text = """
+f(a.attr)
+def func(v):
+    f(v)
+    v2 = 1
+    f(v2)
+    f(b)
+    global c
+    c = 1
+d = 2
+"""
+        self.assertEquals(free_vars(text),
+                          set(["f", "a", "b", "c", "d", "func"]))
+        text = """
+class C(object):
+    x = 0
+    def f(self):
+        return x # global var
+    y = 0
+    z = y # from class scope
+    k = k # from global scope into class scope
+"""
+        # TODO: k should be considered a FV
+        self.assertEquals(free_vars(text), set(["object", "x", "C"]))
+
+    def test_error_comments(self):
+        # Expected errors can be embedded in comments for the purposes
+        # of this test suite.
+        code = """
+blah # FAIL: FooError
+blahh # FAIL: InsufficientCheeseError
+"""
+        self.assertEquals(list(find_expected_errors(code)),
+                          [("FooError", 2), ("InsufficientCheeseError", 3)])
+
+    def check(self, free_vars, code_text):
+        tree = parse_statement(code_text)
+        global_vars = lint.annotate(tree)
+        self.assertEquals(global_vars, set(free_vars))
+        logged = lint.check(tree, free_vars)
+        self.assertEquals(sorted([(error, node.lineno)
+                                  for error, node in logged]),
+                          sorted(find_expected_errors(code_text)))
+
+    def test_check(self):
+        self.check(["a"], """
+a.b = 1 # FAIL: SetAttr
+a._b = 1 # FAIL: SetAttr
+a.b
+a._b # FAIL: GetAttr
+""")
+
+        self.check(["object", "C", "C2"], """
+class C(object):
+    def method(self):
+        self.a = 1
+        self._a = 2
+        self.a
+        self._a
+
+class C2(object):
+    # self variables don't have to be called "self".
+    def method(badger):
+        badger.a = 1
+        badger._a = 2
+        badger.a
+        badger._a
+""")
+
+        self.check(["object", "C", "func"], """
+class C(object):
+    @staticmethod
+    def method(self):
+        # self is not really a self variable here, because of the decorator.
+        self.a = 1 # FAIL: SetAttr
+        self._a = 2 # FAIL: SetAttr
+        self.a
+        self._a # FAIL: GetAttr
+
+def func(self, arg1, arg2):
+    # self is not really a self variable here either.
+    self.a = 1 # FAIL: SetAttr
+    self._a = 2 # FAIL: SetAttr
+    self.a
+    self._a # FAIL: GetAttr
+""")
+
+    @TODO_test
+    def test_check_2(self):
+        self.check(["C", "object"], """
+class C(object):
+    def method(self):
+        self._private
+    # This should be rejected because it allows the method's function to
+    # escape the class definition.
+    global f
+    f = method # FAIL: MethodEscapes
+    lst = [method] # FAIL: MethodEscapes
+""")
+
+
+if __name__ == "__main__":
+    unittest.main()
