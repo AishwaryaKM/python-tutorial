adc9314dec956725967311d8611ee000173293ab
Malthe Borch mborch@gmail.com <Malthe Borch mborch@gmail.com@8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9>
2008-11-27 11:22:26 +0000

    Exclude change-log.
    git-svn-id: http://svn.repoze.org/playground/malthe/sourcecodegen@2636 8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+# CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(
     name='sourcecodegen',
     version=__version__,
     description='A Python source-code generator based on the ``compiler.ast`` ' + 
                 'abstract syntax tree.',
-    long_description=README + '\n\n' +  CHANGES,
+    long_description=README,
     classifiers=[
        "Development Status :: 3 - Alpha",
        "Intended Audience :: Developers",
