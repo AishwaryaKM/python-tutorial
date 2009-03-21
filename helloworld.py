500704a1cc486eb253690bdafbefd8bc4111b2ed
James Ascroft-Leigh <jwal@jwal-desktop>
2009-03-21 10:39:58 +0000

    In progress: Integrating cappython

--- helloworld.py
+++ helloworld.py
@@ -9,46 +9,65 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-class Greeting(db.Model):
-  author = db.UserProperty()
-  content = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
+import functools
+
+import cappython.pycheck
+
+class UserTag(db.Model):
+    user = db.UserProperty()
+    tag = db.StringProperty()
+
+def has_tag(tag):
+    user = users.get_current_user()
+    user_tags = db.GqlQuery("SELECT * FROM UserTag "
+                            "WHERE user = :1 AND tag = :2", user, tag)
+    result = users.is_current_user_admin() or user_tags.count() > 0
+    if result and user_tags.count() == 0:
+        assert users.is_current_user_admin()
+        db.put(UserTag(user=user, tag=tag))
+    return result
+    
+def requires_tag(tag):
+    def decorator(func):
+        @functools.wraps(func)
+        def handler(self, *args, **kwargs):
+            if has_tag(tag):
+                return func(self, *args, **kwargs)
+            else:
+                if users.get_current_user():
+                    url = users.create_logout_url(self.request.uri)
+                    url_linktext = 'Logout'
+                else:
+                    url = users.create_login_url(self.request.uri)
+                    url_linktext = 'Login'
+                template_values = {
+                    'url': url,
+                    'url_linktext': url_linktext,
+                    }
+                path = os.path.join(os.path.dirname(__file__), 'index.html')
+                self.response.out.write(template.render(path, template_values))
+        return handler
+    return decorator
 
 class MainPage(webapp.RequestHandler):
-  def get(self):
-    greetings_query = Greeting.all().order('-date')
-    greetings = greetings_query.fetch(10)
-
-    if users.get_current_user():
-      url = users.create_logout_url(self.request.uri)
-      url_linktext = 'Logout'
-    else:
-      url = users.create_login_url(self.request.uri)
-      url_linktext = 'Login'
-
-    template_values = {
-      'greetings': greetings,
-      'url': url,
-      'url_linktext': url_linktext,
-      }
-
-    path = os.path.join(os.path.dirname(__file__), 'index.html')
-    self.response.out.write(template.render(path, template_values))
+    
+    @requires_tag("user")
+    def get(self):
+        if users.get_current_user():
+          url = users.create_logout_url(self.request.uri)
+          url_linktext = 'Logout'
+        else:
+          url = users.create_login_url(self.request.uri)
+          url_linktext = 'Login'
+        template_values = {
+          'url': url,
+          'url_linktext': url_linktext,
+          }
+        path = os.path.join(os.path.dirname(__file__), 'repl.html')
+        self.response.out.write(template.render(path, template_values))
 
 
-class Guestbook(webapp.RequestHandler):
-  def post(self):
-    greeting = Greeting()
-
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
-
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/')
-
-application = webapp.WSGIApplication(
-                                     [('/', MainPage),
+application = webapp.WSGIApplication([('/', MainPage),
                                       ('/sign', Guestbook)],
                                      debug=True)
 
